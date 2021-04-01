8eebede7a79ab2e06b279f711664b347de1f661a
Anastasia <anromanova.design@gmail.com>
2021-04-01 18:31:38 +0200

    add info

--- src/GetNames.js
+++ src/GetNames.js
@@ -3,15 +3,19 @@ import React, {useState, useEffect} from "react";
  function GetNames() {
   const [list, setList] = useState([]);
   const [page, setPage] = useState(1);
+  const [displayDetails, setDisplayDetails] = useState([]);
 
   useEffect(() => {
     if (page !== null) {
      fetch(`https://swapi.dev/api/people/?page=${page}`)
        .then(response => response.json())
        .then(data => {
-          console.log(data);
           list.push(...data.results);
           setList(list);
+          for (var i = 0; i < data.results.length; i++) {
+            displayDetails.push(false);
+          }
+          setDisplayDetails(displayDetails);
           if (data.next !== null) {
             setPage(page + 1);
           } else {
@@ -21,19 +25,28 @@ import React, {useState, useEffect} from "react";
     }
   }, [page])
 
-  function handleNameClick(elem) {
-
+  function handleNameClick(i) {
+    setDisplayDetails(state => {
+      const newState = state.map((item, j) => {
+        if (j === i) {
+          return !item;
+        } else {
+          return item;
+        }
+      });
+      return newState;
+    });
   }
+
 
   return (
       <ul>
-        {list.map(elem => 
-          <li key={elem.name} onClick={() => handleNameClick(elem)}>
+        {list.map( (elem, index) => 
+          <li key={elem.name} onClick={() => handleNameClick(index)}>
             {elem.name}
-            <ul>
-              <li>height: {elem.height}</li>
-            </ul>
-          </li>)}
+            {displayDetails[index] ? <ul><li>height: {elem.height}</li></ul> : null}
+          </li>
+        )}
       </ul>
     )
 
